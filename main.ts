ccb7a98bdbe4ee2893cfedf6bb1d9e099aacfe10
Andrey Leshenko <andrey.leshenko@gmail.com>
2023-09-30 17:11:59 +0300

    Initial version of space.ts

--- main.ts
+++ main.ts
@@ -2,31 +2,37 @@ export {}
 
 import {Space} from './space.js'
 
-console.log('hello world')
-
 let canvas: HTMLCanvasElement = document.getElementById('canvas')! as HTMLCanvasElement
 let ctx = canvas.getContext('2d')!
 
 let waitFrame = () => new Promise((resolve,reject) => requestAnimationFrame(resolve))
 
-let space = new Space()
+let space = new Space(ctx)
 
-async function drawFrame() {
+document.addEventListener('mousedown', (e) => {
+    space.mouseDown(e.clientX, e.clientY)
+})
+document.addEventListener('mousemove', (e) => {
+    space.mouseMove(e.clientX, e.clientY)
+})
+
+let previousTimestamp: number;
+
+async function drawFrame(timestamp: number) {
+    let dt = timestamp - previousTimestamp;
+    previousTimestamp = timestamp
+
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-    space.update()
+    space.update(dt)
 }
 
 async function main() {
+    previousTimestamp = await waitFrame() as number
     while (true) {
-        await waitFrame()
-        drawFrame()
+        drawFrame(await waitFrame() as number)
     }
 }
-
-document.addEventListener('mousedown', (e) => {
-    console.log('clicked')
-})
 
 await main()
--- space.ts
+++ space.ts
@@ -1,6 +1,71 @@
+import { Satellite } from './satellite.js'
+import { Point, dist } from './utils.js'
+
 export class Space {
     satellites: Satellite[] = []
+    ctx: CanvasRenderingContext2D
 
-    update() {
+    editedSatellite: Satellite | null = null
+    editedAngle = 0
+
+    spawnSatellite() {
+        this.satellites.push(new Satellite());
+    }
+
+    mouseDown(x: number, y: number) {
+        if (!this.editedSatellite) {
+            for (let s of this.satellites) {
+                if (dist({x, y}, s.getPosAtTime()) < s.radius) {
+                    this.editedSatellite = s
+                    break
+                }
+            }
+        }
+        else {
+            this.editedSatellite.setNewPath(this.editedAngle)
+            this.editedSatellite = null
+        }
+    }
+    mouseMove(x: number, y: number) {
+        if (this.editedSatellite) {
+            let {x:satX, y:satY} = this.editedSatellite.getPosAtTime()
+            this.editedAngle = Math.atan2(x - satX, y - satY)
+        }
+    }
+
+    constructor(ctx: CanvasRenderingContext2D) {
+        this.ctx = ctx
+        this.spawnSatellite()
+    }
+
+    update(dt: number) {
+        if (!this.editedSatellite) {
+            for (let s of this.satellites) {
+                s.update(dt)
+            }
+        }
+
+        // Check for collisions
+        for (let i = 0; i < this.satellites.length; i++) {
+            for (let j = i + 1; j < this.satellites.length; j++) {
+                let pi = this.satellites[i].getPosAtTime()
+                let pj = this.satellites[j].getPosAtTime()
+                if (dist(pi, pj) < this.satellites[i].radius + this.satellites[j].radius) {
+                    alert('BOOM!')
+                }
+            }
+        }
+
+        for (let s of this.satellites) {
+            s.drawPath()
+        }
+
+        if (this.editedSatellite) {
+            this.editedSatellite.previewNewPath(this.editedAngle)
+        }
+
+        for (let s of this.satellites) {
+            s.drawSelf()
+        }
     }
 }
